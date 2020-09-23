7af0585f80a88daf27f6ec59085d0311cd1e98e2
Gonzalo Duval <gonzaloduval95@hotmail.com>
2020-09-23 15:12:05 -0300

    Adding a Service Implementation

---
+++ src/com/gdd/springdemo/service/CustomerService.java
@@ -0,0 +1,11 @@
+package com.gdd.springdemo.service;
+
+import java.util.List;
+
+import com.gdd.springdemo.entity.Customer;
+
+public interface CustomerService {
+		
+	public List<Customer> getCustomers();
+	
+}
