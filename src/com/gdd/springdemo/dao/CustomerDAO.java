27a3bdb614f95e7a1db641438b19ad9d8ff22683
Gonzalo Duval <gonzaloduval95@hotmail.com>
2020-09-23 12:38:48 -0300

    Developing Hibernate DAO

---
+++ src/com/gdd/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,11 @@
+package com.gdd.springdemo.dao;
+
+import java.util.List;
+
+import com.gdd.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+	
+	public List<Customer> getCustomers();
+	
+}
